c3f121ff55d9940241e4a80315f98a90a4ad245f
minchan <kroud153@example.com>
2022-02-28 23:09:50 +0900

    2022.02.28(월) 알고리즘 공부

---
+++ c++/boj/DP/9465.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#define SIZE 100000
+using namespace std;
+
+
+void StickerMax(){
+
+
+}
+
+int main(void) {
+  int n = 0;
+  int T = 0;
+  cin >> T;
+  int sticker[2][SIZE] = {0,};
+  for(int i = 0; i <T; i++){
+    for(int i =0; i)
+
+  }
+
+
+  return 0;
+}
