505e1edb9a69c170c90422bbc9d8fc71a43b993e
minchan <kroud153@example.com>
2022-03-14 16:28:04 +0900

    2022.03.14(월) 알고리즘 공부

--- c++/boj/DP/2579.cpp
+++ c++/boj/DP/2579.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-
+#define SIZE 301
 using namespace std;
+
+int n;
+int Num[SIZE];
+int DP[SIZE];
+
+int solution(){
+  DP[1] = Num[1];
+  DP[2] = Num[1] + Num[2];
+  DP[3] = max(Num[1] + Num[3], Num[2]+Num[3]);
+  for(int i = 4; i <= n; i++){
+    DP[i] = max(DP[i-2] + Num[i], DP[i-3]+Num[i-1]+Num[i]);
+  }
+
+  return DP[n];
+}
 
 int main(void){
 
-  int n = 0;
+  scanf("%d",&n);
+  for(int i = 1; i<= n; i++){
+    scanf("%d",&Num[i]);
+  }
 
+  printf("%d",solution());
   return 0;
 }
