658baf192871c3d8be458d0b4018c216c5b89a65
minchan <kroud153@example.com>
2022-03-16 22:01:27 +0900

    2022.03.16(수) 알고리즘 공부

--- c++/boj/DP/1699.cpp
+++ c++/boj/DP/1699.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-
+#include <algorithm>
+#define SIZE 100001
 using namespace std;
 
+int n;
+int dp[SIZE] = {0,};
+
 int main(void){
+  scanf("%d",&n);
+  for(int i = 1; i <= n; i++){
+    dp[i] = i;
+    for(int j = 1; j*j <= i; j++){
+      dp[i] = min(dp[i],dp[i-j^2]+1);
+    }
+  }
+
+  printf("%d",dp[n]);
 
   return 0;
 }
