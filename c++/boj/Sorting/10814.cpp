011b09dd8fb1866107f0cb15415c746530e88fdf
minchan <kroud153@example.com>
2022-03-24 01:36:45 +0900

    2022.03.23(화) 알고리즘 공부

--- c++/boj/Sorting/10814.cpp
+++ c++/boj/Sorting/10814.cpp
@@ -11,31 +11,51 @@ struct people{
   string name;
 }typedef P;
 
-bool compare(P a,P b){
+// bool compare(pair<int, string> a, pair<int, string> b){
+//   return a.first < b.first;
+// }
+
+bool comp(P a, P b){
   if(a.age == b.age) return a.order < b.order;
   else return a.age < b.age;
 }
 
+
+//vector<pair<int,string>> v;
 vector<P> v;
 int main(void){
 
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
   int n = 0;
+  // int a;
+  // string b;
   scanf("%d",&n);
 
   for(int i = 1; i <= n; i++){
+    
+    //cin >> a >> b;
+    //v.push_back({a,b});
     P n;
     cin >> n.age >> n.name;
     //cin >> num[i].age >> num[i].name;
     //num[i].order = i;
     n.order = i;
     v.push_back(n);
+    
   }
 
   //sort(num+1, num+n+1,compare);
   // for(int i = 1; i <= n; i++){
   //   cout << num[i].age << " " << num[i].name << endl;
   // }
-  sort(v.begin(),v.end(),compare);
+  sort(v.begin(),v.end(),comp);
+  //stable_sort(v.begin(),v.end(),compare);
+
+  // for(auto c : v){
+  //   cout << c.first << " " << c.second << endl;
+  // }
 
   for(auto c : v){
     cout << c.age << " " << c.name << endl;
---
+++ c++/boj/Sorting/10825.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct student{
+  int kor;
+  int eng;
+  int math;
+  string name;
+} typedef STD;
+
+int comp(STD a, STD b){
+  if(a.kor == b.kor && a.eng == b.eng && a.math == b.math) return a.name < b.name;
+  else if(a.kor == b.kor && a.eng == b.eng) return a.math > b.math;
+  else if(a.kor == b.kor) return a.eng < b.eng;
+  else return a.kor > b.kor;
+}
+
+int main(void){
+
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  vector<STD> v(n);
+  for(int i = 0; i < n; i++){
+    cin >> v[i].name >> v[i].kor >> v[i].eng >> v[i].math;
+  }
+
+  sort(v.begin(), v.end(),comp);
+
+  for(auto c : v){
+    cout << c.name << endl;
+  }
+
+return 0;
+
+}
