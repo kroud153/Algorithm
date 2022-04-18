401f31bbcef8b3f51052369e89156d7c482ad50b
kewrun <kroud153@gmail.com>
2022-04-19 04:01:56 +0900

    2022.04.09(월) 알고리즘 공부

---
+++ c++/boj/Linkedlist/1158.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+
+  int N, K = 0;
+  int list[N] = {0,};
+
+  for(int i = 1; i <= N; i++){
+    list[i-1] = i;
+  }
+
+  cin >> N >> K;
+
+  for(int i = 0; i < N; i++){
+
+  }
+
+  return 0;
+}
