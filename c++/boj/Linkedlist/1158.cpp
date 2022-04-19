b9cefcdead36d00e4d2daa91015857d404b677b6
kewrun <kroud153@gmail.com>
2022-04-19 16:16:17 +0900

    2022.04.19(화) 알고리즘 공부

--- c++/boj/Linkedlist/1158.cpp
+++ c++/boj/Linkedlist/1158.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
-#include <string>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
 int main(void){
 
   int N, K = 0;
-  int list[N] = {0,};
-
-  for(int i = 1; i <= N; i++){
-    list[i-1] = i;
-  }
-
   cin >> N >> K;
+  vector <int> v;
 
   for(int i = 0; i < N; i++){
-
+    v.push_back(i+1);
+    //cout << v[i] << "\n";
   }
+  int temp = 0;
+  cout << "<";
+  for(int i = 0; i < N; i++){
+    
+    temp = (temp + K-1) % v.size();
+    
+    if(i == N-1) cout << v[temp];
+    else cout << v[temp] << ", ";
+    v.erase(v.begin()+temp);
+  }
+  cout << ">";
 
   return 0;
 }
