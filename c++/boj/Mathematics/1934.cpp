5f17661e8a6f83a53dda2e97f1081663ca9ef1db
kewrun <kroud153@gmail.com>
2022-04-28 04:20:41 +0900

    2022.04.27(수) 알고리즘 공부

--- c++/boj/Mathematics/1934.cpp
+++ c++/boj/Mathematics/1934.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+int gcd(int a, int b){
+  int c = a % b;
+  while(c != 0){
+    a = b;
+    b = c;
+    c = a % b;
+  }
+
+  return b;
+}
+
+int lcm (int a , int b){
+  return (a * b) / gcd(a,b);
+}
+
+
 int main(void){
 
   int a, b = 0;
+  int n = 0;
+  cin >> n;
+
+  while(n--){
+    cin >> a >> b;
+    cout << lcm(a,b) << "\n";
+  }
   
 
   return 0;
